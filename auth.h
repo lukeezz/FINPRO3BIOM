517d1310a822922484cdd9f0e653051120b814ab
lukeezz <spplukas@gmail.com>
2025-05-27 22:53:48 +0700

    Selamat Malam

--- auth.h
+++ auth.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 typedef struct {
     char username[50];
@@ -13,13 +14,31 @@ typedef struct {
 
 int loginMenu();
 
+int isValidPassword(const char *password) {
+    int len = strlen(password);
+    if (len < 8) return 0;
+
+    int hasUpper = 0, hasDigit = 0;
+    for (int i = 0; i < len; i++) {
+        if (isupper(password[i])) hasUpper = 1;
+        if (isdigit(password[i])) hasDigit = 1;
+    }
+
+    return hasUpper && hasDigit;
+}
+
 int login() {
     char username[50], password[50];
     FILE *file = fopen("users.txt", "rb");
     if (!file) return 0;
     User u;
-    printf("Username: "); gets(username);
-    printf("Password: "); gets(password);
+
+    printf("Username: "); fgets(username, sizeof(username), stdin);
+    printf("Password: "); fgets(password, sizeof(password), stdin);
+
+    username[strcspn(username, "\n")] = '\0';
+    password[strcspn(password, "\n")] = '\0';
+
     while (fread(&u, sizeof(User), 1, file)) {
         if (strcmp(u.username, username) == 0 && strcmp(u.password, password) == 0) {
             fclose(file);
@@ -33,8 +52,20 @@ int login() {
 
 void registerUser() {
     User u;
-    printf("New username: "); gets(u.username);
-    printf("New password: "); gets(u.password);
+    char tempPassword[50];
+    printf("New username: "); fgets(u.username, sizeof(u.username), stdin);
+    u.username[strcspn(u.username, "\n")] = '\0';
+
+    do {
+        printf("New password: "); fgets(tempPassword, sizeof(tempPassword), stdin);
+        tempPassword[strcspn(tempPassword, "\n")] = '\0';
+        if (!isValidPassword(tempPassword)) {
+            printf("Password must be at least 8 characters long, contain one uppercase letter, and one digit.\n");
+        }
+    } while (!isValidPassword(tempPassword));
+    
+    strcpy(u.password, tempPassword);
+
     printf("Is Manager? (1=Yes, 0=No): "); scanf("%d", &u.isManager); getchar();
     FILE *file = fopen("users.txt", "ab");
     fwrite(&u, sizeof(User), 1, file);
@@ -54,6 +85,5 @@ int loginMenu() {
         }
     } while (1);
 }
-
 
 #endif
